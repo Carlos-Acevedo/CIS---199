e2d9bca4008278bd6977efd8170f8f03a037d6d2
Carlos-Acevedo <35580832+Carlos-Acevedo@users.noreply.github.com>
2018-11-29 01:30:18 -0500

    Add files via upload

---
+++ Prog1B/Prog1/DescendingOrderCopyright.cs
@@ -0,0 +1,33 @@
+﻿// Program 4
+// CIS 200-01
+// Due: 4/19/2017
+// By: D6571
+
+// File: DescendingOrderCopyright
+// compares the Library Items by Copyright year and returns them in descending order
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryItems
+{
+    // Compares by CopyrightYear
+    public class DescendingOrderCopyright : Comparer<LibraryItem>
+    {
+        // Precondition:  None
+        // Postcondition: Reverses natural time order, so descending
+        //                When x < y, method returns positive #
+        //                When x == y, method returns zero
+        //                When x > y, method returns negative #
+        public override int Compare(LibraryItem x, LibraryItem y)
+        {
+            if (x.CopyrightYear.CompareTo(y.CopyrightYear) != 0)    // Different CopyrightYear?
+            {
+                return x.CopyrightYear.CompareTo(y.CopyrightYear);  // Use CopyrightYear to decide
+            }
+            else
+                return (-1) * x.CompareTo(y); // Reverses natural order, so descending
+        }
+    }
+}
