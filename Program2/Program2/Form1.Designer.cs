98ec237bbf5140aa8e14df0a3413c98cd67c30bc
Carlos-Acevedo <35580832+Carlos-Acevedo@users.noreply.github.com>
2018-11-29 01:21:13 -0500

    Add files via upload

---
+++ Program2/Program2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Program2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.priorityERSC_LBL = new System.Windows.Forms.Label();
+            this.lastName_LBL = new System.Windows.Forms.Label();
+            this.creditHours_LBL = new System.Windows.Forms.Label();
+            this.lastName_TXT = new System.Windows.Forms.TextBox();
+            this.creditHours_TXT = new System.Windows.Forms.TextBox();
+            this.oK_BTN = new System.Windows.Forms.Button();
+            this.scheduleOutPut_LBL = new System.Windows.Forms.Label();
+            this.clear_BTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // priorityERSC_LBL
+            // 
+            this.priorityERSC_LBL.AutoSize = true;
+            this.priorityERSC_LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priorityERSC_LBL.Location = new System.Drawing.Point(68, 29);
+            this.priorityERSC_LBL.Name = "priorityERSC_LBL";
+            this.priorityERSC_LBL.Size = new System.Drawing.Size(502, 24);
+            this.priorityERSC_LBL.TabIndex = 0;
+            this.priorityERSC_LBL.Text = "Priority Early Registration Schedule for Continuing Students";
+            // 
+            // lastName_LBL
+            // 
+            this.lastName_LBL.AutoSize = true;
+            this.lastName_LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastName_LBL.Location = new System.Drawing.Point(229, 100);
+            this.lastName_LBL.Name = "lastName_LBL";
+            this.lastName_LBL.Size = new System.Drawing.Size(84, 18);
+            this.lastName_LBL.TabIndex = 1;
+            this.lastName_LBL.Text = "Last Name:";
+            // 
+            // creditHours_LBL
+            // 
+            this.creditHours_LBL.AutoSize = true;
+            this.creditHours_LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.creditHours_LBL.Location = new System.Drawing.Point(217, 133);
+            this.creditHours_LBL.Name = "creditHours_LBL";
+            this.creditHours_LBL.Size = new System.Drawing.Size(96, 18);
+            this.creditHours_LBL.TabIndex = 2;
+            this.creditHours_LBL.Text = "Credit Hours:";
+            // 
+            // lastName_TXT
+            // 
+            this.lastName_TXT.Location = new System.Drawing.Point(319, 101);
+            this.lastName_TXT.Name = "lastName_TXT";
+            this.lastName_TXT.Size = new System.Drawing.Size(100, 20);
+            this.lastName_TXT.TabIndex = 3;
+            // 
+            // creditHours_TXT
+            // 
+            this.creditHours_TXT.Location = new System.Drawing.Point(319, 134);
+            this.creditHours_TXT.Name = "creditHours_TXT";
+            this.creditHours_TXT.Size = new System.Drawing.Size(100, 20);
+            this.creditHours_TXT.TabIndex = 4;
+            // 
+            // oK_BTN
+            // 
+            this.oK_BTN.Location = new System.Drawing.Point(283, 182);
+            this.oK_BTN.Name = "oK_BTN";
+            this.oK_BTN.Size = new System.Drawing.Size(75, 23);
+            this.oK_BTN.TabIndex = 5;
+            this.oK_BTN.Text = "OK";
+            this.oK_BTN.UseVisualStyleBackColor = true;
+            this.oK_BTN.Click += new System.EventHandler(this.oK_BTN_Click);
+            // 
+            // scheduleOutPut_LBL
+            // 
+            this.scheduleOutPut_LBL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scheduleOutPut_LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scheduleOutPut_LBL.Location = new System.Drawing.Point(118, 220);
+            this.scheduleOutPut_LBL.Name = "scheduleOutPut_LBL";
+            this.scheduleOutPut_LBL.Size = new System.Drawing.Size(393, 23);
+            this.scheduleOutPut_LBL.TabIndex = 6;
+            this.scheduleOutPut_LBL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clear_BTN
+            // 
+            this.clear_BTN.Location = new System.Drawing.Point(283, 262);
+            this.clear_BTN.Name = "clear_BTN";
+            this.clear_BTN.Size = new System.Drawing.Size(75, 23);
+            this.clear_BTN.TabIndex = 7;
+            this.clear_BTN.Text = "Clear";
+            this.clear_BTN.UseVisualStyleBackColor = true;
+            this.clear_BTN.Click += new System.EventHandler(this.clear_BTN_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.oK_BTN;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.clear_BTN;
+            this.ClientSize = new System.Drawing.Size(630, 322);
+            this.Controls.Add(this.clear_BTN);
+            this.Controls.Add(this.scheduleOutPut_LBL);
+            this.Controls.Add(this.oK_BTN);
+            this.Controls.Add(this.creditHours_TXT);
+            this.Controls.Add(this.lastName_TXT);
+            this.Controls.Add(this.creditHours_LBL);
+            this.Controls.Add(this.lastName_LBL);
+            this.Controls.Add(this.priorityERSC_LBL);
+            this.Name = "Form1";
+            this.Text = "Program 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label priorityERSC_LBL;
+        private System.Windows.Forms.Label lastName_LBL;
+        private System.Windows.Forms.Label creditHours_LBL;
+        private System.Windows.Forms.TextBox lastName_TXT;
+        private System.Windows.Forms.TextBox creditHours_TXT;
+        private System.Windows.Forms.Button oK_BTN;
+        private System.Windows.Forms.Label scheduleOutPut_LBL;
+        private System.Windows.Forms.Button clear_BTN;
+    }
+}
+
---
+++ Program2/Program2/Form1.cs
@@ -0,0 +1,154 @@
+﻿//Grading ID:A9931
+//Program2
+//Due Date: 10/16/2016
+//CIS-199-01
+//Description: The user will enter his/her last name and credit hours and it will output when he/she can register for classes.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string time; //registration time
+        string date; //registration date
+
+        //Show the date and time the user registers when clicking the OK button.
+        private void oK_BTN_Click(object sender, EventArgs e)
+        {
+
+            double creditHours; // Hold number of credit hours.
+            string name = lastName_TXT.Text; // string for the last name texbox input.
+            char firstLetter; // Hold first letter of the name texbox.
+
+
+            //Input
+            firstLetter = char.ToLower(name[0]); //replaces first letter from string and converts it to lowrcase.
+
+            if (double.TryParse(creditHours_TXT.Text, out creditHours)) //Verify data for credit hours input, if incorrect, show error message.
+            {
+                // Registration dates and times for Freshman and Sophomores
+                if(creditHours < 60)
+                {
+                    if(firstLetter >= 'a')
+                    {
+                        time = "10:00am.";
+                    }
+                    if (firstLetter >= 'c')
+                    {
+                        time = "11:30am.";
+                    }
+                    if(firstLetter >= 'e')
+                    {
+                        time = "2:00pm.";
+                    }
+                    if (firstLetter >= 'g')
+                    {
+                        time = "4:00pm.";
+                    }
+                    if (firstLetter >= 'j')
+                    {
+                        time = "8:30am.";
+                    }
+                    if (firstLetter >= 'm')
+                    {
+                        time = "10:00am.";
+                    }
+                    if (firstLetter >= 'p') 
+                    {
+                        time = "11:30am.";
+                    }
+                    if (firstLetter >= 'r')
+                    {
+                        time = "2:00pm.";
+                    }
+                    if (firstLetter >= 't')
+                    {
+                        time = "4:00pm.";
+                    }
+                    if (firstLetter >= 'w')
+                    {
+                        time = "8:30am.";
+                    }
+
+                    //Dates for Sophomores
+                    if (creditHours >= 30)
+                    {
+                        if (firstLetter >= 'j' && firstLetter <= 'v')
+                        {
+                            date = "Wednesday, November 9th";
+                        }
+                        else if (firstLetter <= 'i' || firstLetter >= 'w')
+                        {
+                            date = "Thursday, November 10th";
+                        }
+                    }
+
+                }
+
+                // Registration dates and times for Juniors and Senious
+                if (creditHours >= 60)
+                {
+                    if(firstLetter >= 't')
+                    {
+                        time = "11:30am.";
+                    }
+                    else if (firstLetter >= 'p')
+                    {
+                        time = "10:00am.";
+                    }
+                    else if (firstLetter >= 'j')
+                    {
+                        time = "8:30am.";
+                    }
+                    else if (firstLetter >= 'e')
+                    {
+                        time = "4:00pm.";
+                    }
+                    else if (firstLetter >= 'a')
+                    {
+                        time = "2:00pm.";
+                    }
+                    
+                    date = "Monday, November 7th"; //Registration date for Juniors
+
+                    if (creditHours >= 90)
+                    {
+                        date = "Friday, November 4th"; //Registration date for Seniors
+                    }
+                }
+            }else
+            {
+                MessageBox.Show("Enter a number for Credit Hours"); //Show error message when input incorrect for credit Hours.
+            }
+
+            scheduleOutPut_LBL.Text = date + " " + "at" + " " + time; //Concatenate variables and display output
+
+        }
+
+        //Clear input/output when you click the clear button.
+        private void clear_BTN_Click(object sender, EventArgs e)
+        {
+
+            creditHours_TXT.Clear();//clear input for credit hours
+            lastName_TXT.Clear();//clear input for last name
+            scheduleOutPut_LBL.Text = "";//clear output
+
+            lastName_TXT.Focus();//set focus on last name after clearing input/output.
+        }
+    }
+}
